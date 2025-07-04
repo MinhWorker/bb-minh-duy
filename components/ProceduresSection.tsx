84ae721af4665e1546c7e9968016c6c22ddefb91
Dat Do Duc <doducdat192@gmail.com>
2025-07-04 07:36:58 +0700

    Re-styled procedures image size

--- components/ProceduresSection.tsx
+++ components/ProceduresSection.tsx
@@ -5,7 +5,7 @@ const ProceduresSection = () => {
     <section className="my-12 scroll-mt-28" id="procedures">
       <h1 className="text-primary-foreground text-center font-semibold text-[40px] mb-4 text-shadow-md text-shadow-accent/25">QUY TRÌNH</h1>
 
-      <div className="relative w-full h-[300px] hidden md:block">
+      <div className="relative w-full md:h-[300px] lg:h-[400px] hidden md:block">
         <Image
           src="/images/procedure-web.png"
           alt="Quy trình làm việc"
